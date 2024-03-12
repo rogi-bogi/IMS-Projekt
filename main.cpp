6640d82d90615e881ed1ce6f0f206ee95491dbb8
Kacper <kacper.rogalewski22@gmail.com>
2024-03-12 23:09:47 +0100

    Initial commit :)

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Kacper on 12.03.2024.
+//
+
+#include "main.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "OTO NASZ PROJEKT :)" << endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Kacper on 12.03.2024.
+//
+
+#ifndef IMS_PROJEKT_MAIN_H
+#define IMS_PROJEKT_MAIN_H
+
+#endif //IMS_PROJEKT_MAIN_H
