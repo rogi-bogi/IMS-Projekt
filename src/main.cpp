a0923c1435cc77309b805714ba8d7184fcbf25fa
piotrsamczuk <samczukp@gmail.com>
2024-03-14 12:29:31 +0100

    Add addNoise function

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
+#include <opencv2/core.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 
+void addNoise(cv::Mat& src)
+{
+  if (src.empty())
+  {
+    return;
+  }
+  cv::Mat noise(src.size(), src.type());
+  constexpr auto mean = 5u;
+  constexpr auto var = 20u;
+  cv::randn(noise, mean, var);
+  src += noise;
+}
+
 int main()
 {
-  cv::Mat image;
-  image = cv::imread("../images/lena.png");
-  if (image.empty())
+  cv::Mat inputImage;
+  inputImage = cv::imread("../images/lena.png", cv::IMREAD_GRAYSCALE);
+  if (inputImage.empty())
   {
     return 1;
   }
-  cv::imshow("lena", image);
+  cv::imshow("lena", inputImage);
   cv::waitKey(0);
-  cv::Mat image2;
-  cv::dft(image, image2);
-  if (image2.empty())
-  {
-    return 1;
-  }
-  cv::imshow("image2 after dft", image2);
+
+  cv::Mat outputImage(inputImage);
+  addNoise(outputImage);
+  cv::imshow("lena after noise", outputImage);
+
   cv::waitKey(0);
-  return 0;
 }
