2daf94359d906dd4e7c433c0a2c1377575b1242a
Piotr <samczukp@gmail.com>
2024-06-05 22:53:35 +0200

    Add histogram generator, introduce "fftFilters.hpp" file

---
+++ src/fftFilters.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+
+void idealLpFilter(cv::Mat& scr, cv::Mat& H, float D, float D0)
+{
+  for (int u = 0; u < H.rows; u++)
+  {
+    for (int v = 0; v < H.cols; v++)
+    {
+      D = sqrt((u - scr.rows / 2) * (u - scr.rows / 2) + (v - scr.cols / 2) * (v - scr.cols / 2));
+      if (D > D0)
+      {
+        H.at<float>(u, v) = 0;
+      }
+    }
+  }
+}
+
+void gaussianLpFilter(cv::Mat& scr, cv::Mat& H, float D, float D0)
+{
+  for (int u = 0; u < H.rows; u++)
+  {
+    for (int v = 0; v < H.cols; v++)
+    {
+      D = sqrt((u - scr.rows / 2) * (u - scr.rows / 2) + (v - scr.cols / 2) * (v - scr.cols / 2));
+      H.at<float>(u, v) = exp(-D * D / (2 * D0 * D0));
+    }
+  }
+}
+
+void idealHpFilter(cv::Mat& scr, cv::Mat& H, float D, float D0)
+{
+  for (int u = 0; u < H.rows; u++)
+  {
+    for (int v = 0; v < H.cols; v++)
+    {
+      D = sqrt((u - scr.rows / 2) * (u - scr.rows / 2) + (v - scr.cols / 2) * (v - scr.cols / 2));
+      if (D < D0)
+      {
+        H.at<float>(u, v) = 0;
+      }
+    }
+  }
+}
+
+void gaussianHpFilter(cv::Mat& scr, cv::Mat& H, float D, float D0)
+{
+  for (int u = 0; u < H.rows; u++)
+  {
+    for (int v = 0; v < H.cols; v++)
+    {
+      D = sqrt((u - scr.rows / 2) * (u - scr.rows / 2) + (v - scr.cols / 2) * (v - scr.cols / 2));
+      H.at<float>(u, v) = 1 - exp(-D * D / (2 * D0 * D0));
+    }
+  }
+}
+
+void bandPassFilter(cv::Mat& scr, cv::Mat& H, float D, float D0)
+{
+  float D1 = D0 * 0.75;
+  float D2 = D0 * 1.25;
+  for (int u = 0; u < H.rows; u++)
+  {
+    for (int v = 0; v < H.cols; v++)
+    {
+      D = sqrt((u - scr.rows / 2) * (u - scr.rows / 2) + (v - scr.cols / 2) * (v - scr.cols / 2));
+      if (D < D1 || D > D2)
+      {
+        H.at<float>(u, v) = 0;
+      }
+    }
+  }
+}
+
+void notchFilter(cv::Mat& scr, cv::Mat& H, float D, float D0)
+{
+  float D1 = D0 * 0.75;
+  float D2 = D0 * 1.25;
+  for (int u = 0; u < H.rows; u++)
+  {
+    for (int v = 0; v < H.cols; v++)
+    {
+      D = sqrt((u - scr.rows / 2) * (u - scr.rows / 2) + (v - scr.cols / 2) * (v - scr.cols / 2));
+      if (D >= D1 && D <= D2)
+      {
+        H.at<float>(u, v) = 0;
+      }
+    }
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -34,91 +34,90 @@ int main(int argc, char** argv) {
   }
 */
 
-// int main()
-// {
-//   Mat imgIn = imread("../images/lena.png", IMREAD_GRAYSCALE);
-//   imshow("img", imgIn);
-//   waitKey();
-//   // Converting from 8-bit to float type suitable for DFT
-//   imgIn.convertTo(imgIn, CV_32F);
+int main()
+{
+  Mat imgIn = imread("../images/lena.png", IMREAD_GRAYSCALE);
+  imshow("img", imgIn);
+  waitKey();
+  // Converting from 8-bit to float type suitable for DFT
+  imgIn.convertTo(imgIn, CV_32F);
 
-//   // Calculate DFT
-//   Mat DFT_image;
-//   calculateDFT(imgIn, DFT_image);
-//   show_dft_effect(DFT_image);
+  // Calculate DFT
+  Mat DFT_image;
+  calculateDFT(imgIn, DFT_image);
+  show_dft_effect(DFT_image);
 
-//   while (true)
-//   {
-//     destroyAllWindows();
-//     // Construct H's (filter matrices)
-//     Mat H;
-//     int choice;
-//     float D0;
-//     std::cout << "Choose a filter type (press '0' to exit):\n";
-//     std::cout << "1. Ideal LP\n";
-//     std::cout << "2. Gaussian LP\n";
-//     std::cout << "3. Ideal HP\n";
-//     std::cout << "4. Gaussian HP\n";
-//     std::cout << "5. BandPass\n";
-//     std::cout << "6. Notch\n";
-//     std::cout << "Enter your choice (1-6): ";
-//     std::cin >> choice;
-//     if (choice == 0)
-//     {
-//       break;
-//     }
-//     std::cout << "Enter the desired D0 (0-100 makes sense):\n";
-//     std::cin >> D0;
+  while (true)
+  {
+    destroyAllWindows();
+    // Construct H's (filter matrices)
+    Mat H;
+    int choice;
+    float D0;
+    std::cout << "Choose a filter type (press '0' to exit):\n";
+    std::cout << "1. Ideal LP\n";
+    std::cout << "2. Gaussian LP\n";
+    std::cout << "3. Ideal HP\n";
+    std::cout << "4. Gaussian HP\n";
+    std::cout << "5. BandPass\n";
+    std::cout << "6. Notch\n";
+    std::cout << "Enter your choice (1-6): ";
+    std::cin >> choice;
+    if (choice == 0)
+    {
+      break;
+    }
+    std::cout << "Enter the desired D0 (0-100 makes sense):\n";
+    std::cin >> D0;
 
-//     switch (choice)
-//     {
-//       case 1:
-//         H = construct_H(imgIn, "Ideal LP", static_cast<float>(D0));
-//         break;
-//       case 2:
-//         H = construct_H(imgIn, "Gaussian LP", static_cast<float>(D0));
-//         break;
-//       case 3:
-//         H = construct_H(imgIn, "Ideal HP", static_cast<float>(D0));
-//         break;
-//       case 4:
-//         H = construct_H(imgIn, "Gaussian HP", static_cast<float>(D0));
-//         break;
-//       case 5:
-//         H = construct_H(imgIn, "BandPass", static_cast<float>(D0));
-//         break;
-//       case 6:
-//         H = construct_H(imgIn, "Notch", static_cast<float>(D0));
-//         break;
-//       default:
-//         std::cerr << "Invalid choice\n";
-//         continue;
-//     }
+    switch (choice)
+    {
+      case 1:
+        H = construct_H(imgIn, "Ideal LP", static_cast<float>(D0));
+        break;
+      case 2:
+        H = construct_H(imgIn, "Gaussian LP", static_cast<float>(D0));
+        break;
+      case 3:
+        H = construct_H(imgIn, "Ideal HP", static_cast<float>(D0));
+        break;
+      case 4:
+        H = construct_H(imgIn, "Gaussian HP", static_cast<float>(D0));
+        break;
+      case 5:
+        H = construct_H(imgIn, "BandPass", static_cast<float>(D0));
+        break;
+      case 6:
+        H = construct_H(imgIn, "Notch", static_cast<float>(D0));
+        break;
+      default:
+        std::cerr << "Invalid choice\n";
+        continue;
+    }
 
-//     // Apply filtering and display the frequency domain
-//     Mat filtered_img;
-//     filtering(DFT_image, filtered_img, H);
-//     show_dft_effect(filtered_img);
+    // Apply filtering and display the frequency domain
+    Mat filtered_img;
+    filtering(DFT_image, filtered_img, H);
+    show_dft_effect(filtered_img);
 
-//     // Doing a reversed DFT to visualize final effect
-//     Mat imgOut = reverseDTF(filtered_img);
-//     imshow("Filtered Image", imgOut);
+    // Doing a reversed DFT to visualize final effect
+    Mat imgOut = reverseDTF(filtered_img);
+    imshow("Filtered Image", imgOut);
 
-//     if (waitKey(0) == '0')
-//       break;
-//   }
+    if (waitKey(0) == '0')
+      break;
+  }
 
-//   return 0;
-// }
-
+  return 0;
+}
 
 // WAVELETS IMPLEMENTATION
-int main(int ac, char** av) 
-{
-    VideoCapture capture(0);
-    if (!capture.isOpened()) 
-    {
-        return 1;
-    }
-    return process(capture);
-}
+// int main(int ac, char** av)
+// {
+//     VideoCapture capture(0);
+//     if (!capture.isOpened())
+//     {
+//         return 1;
+//     }
+//     return process(capture);
+// }
